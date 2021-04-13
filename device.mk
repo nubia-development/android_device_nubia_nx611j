89f4b6fa785274819eaf20c213441bfec2df451d
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    nx611j: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- device.mk
+++ device.mk
@@ -76,7 +76,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32
 
 PRODUCT_PACKAGES += \
