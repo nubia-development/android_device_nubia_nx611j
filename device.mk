33d37c365ceaa0cf57cae3641d1945ad8b5c81c2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-21 22:15:07 +0200

    nx611j: Use QTI USB init configs from USB HAL repo
    Change-Id: I966d251a2756c9d9e91f621cbc5872448d022de4

--- device.mk
+++ device.mk
@@ -264,13 +264,10 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
@@ -420,7 +417,11 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Verity
 $(call inherit-product, build/target/product/verity.mk)
