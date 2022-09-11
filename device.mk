9c3026a76d49a08f13971de4547812e74b9c12ae
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 01:24:51 +0200

    nx611j: Update Widevine service from S62Pro
    Change-Id: I17fd646a6ae638f6486fa0aee0201bec44ad4ec3

--- device.mk
+++ device.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1.vendor
+    android.hardware.drm@1.3.vendor
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
